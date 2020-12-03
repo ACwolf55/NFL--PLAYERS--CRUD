115b78d65437e21272f4dbd54942c14bb831e24f
Alex Crowe <alexcwolf5@gmail.com>
2020-12-03 16:35:36 -0700

    orignal commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+
+const ctrl = require('./controllers')
+
+app.use(express.json())
+
+ app.get('/nflplayers',ctrl.getAllPlayers)
+ app.get('/nflplayers/:id',ctrl.getPlayer)
+ app.get('/test',ctrl.test)
+ app.post('/nflplayers',ctrl.addPlayer)
+ app.put('/nflplayers/:id',ctrl.editPlayer)
+ app.delete('/nflplayers/:id',ctrl.deletePlayer)
+
+app.listen(PORT, ()=> console.log(`Its working-> PORT: ${PORT}`))
+
